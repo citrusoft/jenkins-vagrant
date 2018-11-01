df0e9c6feb863cc0ad8dae220c0d96f13b6a0ae1
Tommy <citrusoft@gmail.com>
2018-11-01 11:13:30 -0400

    comment out the destroy

--- vagrant_ubuntu.bash
+++ vagrant_ubuntu.bash
@@ -4,4 +4,4 @@ export PATH=$PATH:/usr/local/bin:$PATH
 #vagrant init bento/ubuntu-16.04
 vagrant up --provider=virtualbox
 vagrant ssh -c "top -b -n 1 | head -n 5"
-vagrant destroy --force
+#vagrant destroy --force
